359314329894cc0b79013e792695f0e2195a5f8b
terrobyte-mg <brayanbacari@gmail.com>
2026-05-22 15:58:18 +0300

    liaison page inscription

--- comptes/views.py
+++ comptes/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+from .forms import RegisterForm
+
+
+def register(request):
+
+    if request.method == 'POST':
+
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            messages.success(
+                request,
+                "Votre compte a été créé avec succès. Connectez-vous maintenant."
+            )
+
+            return redirect('login')
+
+    else:
+
+        form = RegisterForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'comptes/register.html', context)
