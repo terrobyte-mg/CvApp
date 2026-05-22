7fb2bcf9c8a9ca1d0a51aa9fa95e0dcc0304e531
terrobyte-mg <brayanbacari@gmail.com>
2026-05-22 14:03:18 +0300

    Accueil fini

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,5 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STATIC_URL = 'static/'
